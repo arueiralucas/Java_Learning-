199b53022d8bca03e203c189926ce6864c50e597
Lucas Ribeiro <108464429+arueiralucas@users.noreply.github.com>
2023-08-15 21:41:30 -0300

    ProgII

---
+++ Atividade3q1_RT.java
@@ -0,0 +1,13 @@
+public class TesteRetangulo {
+    public static void main(String[] args) {
+        Retangulo r = new Retangulo();
+        r.setBase(50);
+        r.setAltura(4);
+        double area = r.calcularArea();
+        double perimetro = r.calcularPerimetro();
+        double diagonal  = r.calcularDiagonal();
+        System.out.println(area);
+        System.out.println(perimetro);
+        System.out.println(diagonal);
+    }
+}
